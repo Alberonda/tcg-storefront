ea57fec776a8778843e9f90f96b2175344683e8c
Alberto Reyes Martin <albertoreyesmartin@gmail.com>
2022-05-17 20:29:35 +0200

    added adapter to fragment view

--- app/src/main/java/com/alberonda/tcgstorefront/view/MainFragment.kt
+++ app/src/main/java/com/alberonda/tcgstorefront/view/MainFragment.kt
@@ -5,14 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.alberonda.tcgstorefront.R
+import com.alberonda.tcgstorefront.databinding.FragmentMainBinding
+import com.alberonda.tcgstorefront.model.data.Game
+import com.alberonda.tcgstorefront.view.adapters.GameAdapter
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_main, container, false
+        )
+
+        val games = listOf(Game("Magic"), Game("Digimon"))
+
+        createAdapter(games)
+
+        return binding.root
+    }
+
+    private fun createAdapter(games: List<Game>) {
+        val recyclerView = binding.gamesRecycler
+
+        recyclerView.adapter = GameAdapter(requireContext(), games)
     }
 }
