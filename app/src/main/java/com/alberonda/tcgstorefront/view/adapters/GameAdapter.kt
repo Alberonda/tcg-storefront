57a707dcf64acbdabc3522f65ad8a1734f592fa1
Alberto Reyes Martin <albertoreyesmartin@gmail.com>
2022-05-17 20:28:48 +0200

    Created GameAdapter

---
+++ app/src/main/java/com/alberonda/tcgstorefront/view/adapters/GameAdapter.kt
@@ -0,0 +1,35 @@
+package com.alberonda.tcgstorefront.view.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.alberonda.tcgstorefront.R
+import com.alberonda.tcgstorefront.model.data.Game
+
+class GameAdapter(
+    private val context: Context,
+    private val dataset: List<Game>
+): RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.game_layout, parent, false)
+
+        return GameViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        val item = dataset[position]
+
+        holder.gameNameView.text = item.name
+    }
+
+    override fun getItemCount() = dataset.size
+
+    class GameViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val gameNameView: TextView = view.findViewById(R.id.game_name_text)
+    }
+}
