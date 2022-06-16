1e34a00f57f613d56b1d5fe379c3af8295f8a880
Alberto Reyes Martin <albertoreyesmartin@gmail.com>
2022-06-16 07:36:29 +0200

    modified game data class to map server response

--- app/src/main/java/com/alberonda/tcgstorefront/model/data/Game.kt
+++ app/src/main/java/com/alberonda/tcgstorefront/model/data/Game.kt
@@ -1,5 +1,8 @@
 package com.alberonda.tcgstorefront.model.data
 
+import com.squareup.moshi.Json
+
 data class Game(
-    val name: String
+    val id: Int,
+    @Json(name = "display_name") val name: String
 )
